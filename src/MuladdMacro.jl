1319a0b80dadb8da16decf35d64dc03e68225041
Hendrik Ranocha <mail@ranocha.de>
2022-05-06 13:37:00 +0200

    extend docstring of at-muladd

--- src/MuladdMacro.jl
+++ src/MuladdMacro.jl
@@ -21,9 +21,10 @@ postwalk(f, x) = f(x)
 postwalk(f, expr::Expr) = f(Expr(expr.head, (postwalk(f, arg) for arg in expr.args)...))
 
 """
-    @muladd
+    @muladd ex
 
-Convert every combination of addition/subtraction and multiplication to a call of `muladd`.
+Convert every combination of addition/subtraction and multiplication in
+expression `ex` to a call of `muladd`.
 
 If both of the involved operators are dotted, `muladd` is applied as a dot call.
 The order of summation might be changed.
